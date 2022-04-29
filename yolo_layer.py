ee698da35906b8f8f200eaec1dc539ec1c123946
Nordin Belkacemi <belkaceminordin@gmail.com>
2022-04-29 11:01:21 +0200

    printing

--- yolo_layer.py
+++ yolo_layer.py
@@ -102,6 +102,7 @@ class YoloLayer(nn.Module):
             conf_mask_false = conf_mask ^ mask
 
             print(int(torch.isnan(x[mask].cpu().detach()).sum().item()))
+            print(int(torch.isnan(tbox[mask][:, 0].cpu().detach()).sum().item()))
             # loss_box = self.ciou_loss(pred_box[mask], tbox[mask])
             loss_x = self.mse_loss(x[mask], tbox[mask][:, 0])
             loss_y = self.mse_loss(y[mask], tbox[mask][:, 1])
