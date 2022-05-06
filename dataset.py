d0adfcf485e7bac9560b1e4e6f24e03866ae4c18
Nordin Belkacemi <belkaceminordin@gmail.com>
2022-05-07 01:14:11 +0200

    back to the basics

--- dataset.py
+++ dataset.py
@@ -21,17 +21,51 @@ class SquarePad:
         return F.pad(img, padding, 0, 'constant')
 
 class ListDataset(Dataset):
-    def __init__(self, list_path, output_img_size = 608):
+    def __init__(self, list_path, anchors, anchor_img_size, model_img_size, tiny = False):
+        #---------
+        #  Data
+        #---------
+
         self.img_files = [list_path + img for img in glob.glob1(list_path, "*.png")]
         self.label_files = [path.replace('images', 'labels').replace('.png', '.txt').replace('.jpg', '.txt') for path in self.img_files]
-        self.img_size = output_img_size
+       
+        if tiny:
+            # for random selection without replacement, conversion to np array is needed
+            img_files = np.array(self.img_files)
+            label_files = np.array(self.label_files)
+            
+            # select 4 elements at random without replacement
+            sel_idx = np.random.choice(len(self.img_files), size = 4, replace = False)
+            self.img_files = img_files[sel_idx].tolist()
+            self.label_files = label_files[sel_idx].tolist()
+        
         self.transform = transforms.Compose([
             SquarePad(),
-            transforms.Resize(output_img_size),
+            transforms.Resize(model_img_size),
             transforms.ToTensor(),
             transforms.Normalize((0.4051, 0.4807, 0.4872),
                                  (0.2181, 0.0336, 0.0453))
         ])
+
+        #---------
+        #  Anchors and anchor masks
+        #---------
+        
+        # rescale anchors to the model image size
+        rescaled_anchors = np.zeros((len(anchors), 2))
+        rescaled_anchors[:, 0] = np.array(anchors)[:, 0] * (model_img_size / anchor_img_size[0])
+        rescaled_anchors[:, 1] = np.array(anchors)[:, 1] * (model_img_size / anchor_img_size[1])
+        self.anchors = rescaled_anchors.tolist()
+
+        # anchor masks for small, medium and large anchors
+        step = len(anchors) // 3
+        self.anchor_masks = [
+            [i for i in range(0 * step, 1 * step)],
+            [i for i in range(1 * step, 2 * step)],
+            [i for i in range(2 * step, 3 * step)]
+        ]
+
+        self.model_img_size = model_img_size
 
     def __getitem__(self, index):
         """
@@ -40,7 +74,6 @@ class ListDataset(Dataset):
             - input_img: the image itself as a pytorch tensor
             - filled_labels: a pytorch tensor (with shape (50, 5)) of labels 
         """
-
         #---------
         #  Image
         #---------
@@ -69,15 +102,15 @@ class ListDataset(Dataset):
         cls = np.copy(labels[:, 0])
     
         pad_left, pad_top, pad_right, pad_bottom = get_square_padding(img)
-        new_size = img_w + pad_left + pad_right
+        sqare_size = img_w + pad_left + pad_right
 
         x_padded, y_padded = (x + pad_left), (y + pad_top)
         w_padded, h_padded = w, h
 
-        x = x_padded * self.img_size / new_size
-        y = y_padded * self.img_size / new_size
-        w = w_padded * self.img_size / new_size
-        h = h_padded * self.img_size / new_size
+        x = x_padded * (self.model_img_size / sqare_size)
+        y = y_padded * (self.model_img_size / sqare_size)
+        w = w_padded * (self.model_img_size / sqare_size)
+        h = h_padded * (self.model_img_size / sqare_size)
 
         labels_scaled = np.zeros((num_labels, 5))
         labels_scaled[:, 0] = x - w / 2    # x1
